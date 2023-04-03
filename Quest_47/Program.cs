a1e38007ca64b91cb3d1e5ea1432a95efa9f44b6
Danil Muzafarov <d.muzafarov95@mail.ru>
2023-04-03 20:05:28 +0300

    Вещественный двумерный рандомный массив

---
+++ Quest_47/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами
+
+Console.Write("Введите количество строк: ");
+int rows = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите количество столбцов: ");
+int columns = int.Parse(Console.ReadLine()!);
+
+double[,] array = GetArray(rows,columns, 0, 99);
+PrintArray(array);
+
+// ----------------Заполнение массива
+double[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    double[,] res = new double[m,n];
+
+    for (int i = 0; i < m; i++)
+    {
+      for(int j = 0; j < n; j++)
+      {
+        res[i, j] = new Random().Next(minValue, maxValue);
+        res[i, j] += Math.Round(new Random().NextDouble(), 2);
+      }
+    }
+    return res;
+}
+
+// -----------------Вывод массива-----------------
+void PrintArray(double[,] array){
+    for(int i = 0; i < array.GetLength(0); i++){
+        for(int j = 0; j < array.GetLength(1); j++){
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
